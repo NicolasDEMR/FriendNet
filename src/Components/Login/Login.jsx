1c992a69e8e2029d0dfcd38bb4e1865c8692d2ca
Nicolas Denamur <nicolas.denamurr@gmail.com>
2023-05-19 18:30:37 +0200

    position login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,49 +5,51 @@ import Footer from "../../Layouts/Footer/Footer";
 function Login() {
   return (
     <div>
-      <div className="menuWrapper">
+      <div className='menuWrapper'>
         <Menu />
       </div>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
+      <div className='d-flex justify-content-center'>
+        <form className='d-flex flex-column w-50 justify-content-center'>
+          <div className='mb-3'>
+            <label htmlFor='exampleInputEmail1' className='form-label'>
+              Email address
+            </label>
+            <input
+              type='email'
+              className='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
+            />
+            <div id='emailHelp' className='form-text'>
+              We'll never share your email with anyone else.
+            </div>
           </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-      <div className="footerWrapper">
+          <div className='mb-3'>
+            <label htmlFor='exampleInputPassword1' className='form-label'>
+              Password
+            </label>
+            <input
+              type='password'
+              className='form-control'
+              id='exampleInputPassword1'
+            />
+          </div>
+          <div className='mb-3 form-check'>
+            <input
+              type='checkbox'
+              className='form-check-input'
+              id='exampleCheck1'
+            />
+            <label className='form-check-label' htmlFor='exampleCheck1'>
+              Check me out
+            </label>
+          </div>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className='footerWrapper'>
         <Footer />
       </div>
     </div>
