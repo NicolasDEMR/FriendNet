86fd34401931d23cffe2bc08fdf696695325f87e
NicolasDEMR <nicolas.denamurr@gmail.com>
2023-05-22 09:53:11 +0200

    bg color

--- src/Layouts/Post/Post.jsx
+++ src/Layouts/Post/Post.jsx
@@ -2,14 +2,14 @@ import "./Post.css";
 
 function Post(props) {
   return (
-    <div className="containerPost">
+    <div className="containerPost bg-black ms-5 me-5">
       <h3>{props.title}</h3>
       <p>{props.content}</p>
       <p>
         <i>{props.author}</i>
       </p>
       <button
-        className="border border-0 rounded-pill "
+        className="border border-0 rounded-pill bg-white  "
         onClick={props.handleClick}
       >
         👍{props.like}
