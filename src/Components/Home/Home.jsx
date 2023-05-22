ebcb82cf8bc057a3abbc624cf0259b4db7b30a3f
TheoCRLDS <theo.carladous@laposte.net>
2023-05-22 09:24:28 +0200

    début composant Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,55 @@
 import "./Home.css";
 import Menu from "../../Layouts/Menu/Menu";
 import Footer from "../../Layouts/Footer/Footer";
-
+import { useEffect, useState } from "react";
+import Post from "../../Layouts/Post/Post";
 function Home() {
+  const [post, setPost] = useState([
+    {
+      title: "My rocket",
+      content: "Look, I built a new rocket",
+      author: "Elon Musk",
+      likes: 0,
+    },
+    {
+      title: "My rocket",
+      content: "Look, I built a new rocket",
+      author: "Elon Musk",
+      likes: 0,
+    },
+    {
+      title: "My rocket",
+      content: "Look, I built a new rocket",
+      author: "Elon Musk",
+      likes: 0,
+    },
+  ]);
+
+  const updateLike = (key) => {
+    setPost([...post], (post[key].likes += 1));
+  };
+
+  const displayPost = () => {
+    return post.map((e, key) => {
+      return (
+        <Post
+          key={key}
+          title={e.title}
+          content={e.content}
+          author={e.author}
+          like={e.likes}
+          handleClick={() => updateLike(key)}
+        />
+      );
+    });
+  };
+
   return (
     <div>
       <div className="menuWrapper">
         <Menu />
       </div>
+      <div className="containerApp">{displayPost()}</div>;
       <div className="footerWrapper">
         <Footer />
       </div>
