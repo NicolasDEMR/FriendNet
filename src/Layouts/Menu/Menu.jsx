8dea0d2427dbcdcb7e438543cf2b98c0f1d50f13
TheoCRLDS <theo.carladous@laposte.net>
2023-05-24 15:11:02 +0200

    Menu amélioré

--- src/Layouts/Menu/Menu.jsx
+++ src/Layouts/Menu/Menu.jsx
@@ -86,23 +86,24 @@ function Menu() {
             </button>
           </Link>
         )}
-        <Link to="/register">
-          <button className="btn btn-dark">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-save"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
-            </svg>
-          </button>
-        </Link>
         {localStorage.getItem("token") === null ? (
-          <div></div>
-        ) : (
+          <Link to="/register">
+            <button className="btn btn-dark">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-save"
+                viewBox="0 0 16 16"
+              >
+                <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
+              </svg>
+            </button>
+          </Link>
+        ) : null}
+
+        {localStorage.getItem("token") === null ? null : (
           <Link to="/settings">
             <button className="btn btn-dark">
               <svg
@@ -119,9 +120,7 @@ function Menu() {
             </button>
           </Link>
         )}
-        {localStorage.getItem("token") === null ? (
-          <div></div>
-        ) : (
+        {localStorage.getItem("token") === null ? null : (
           <Link to="/user">
             <button className="btn btn-dark">
               <svg
