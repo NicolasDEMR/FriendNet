65310fb72559fa5880aaea15955045f425e67da2
TheoCRLDS <theo.carladous@laposte.net>
2023-05-24 14:21:35 +0200

    User v1

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,12 +1,43 @@
 import "./User.css";
 import Menu from "../../Layouts/Menu/Menu";
 import Footer from "../../Layouts/Footer/Footer";
+import { useEffect, useState } from "react";
 
 function User() {
+  const [user, setUser] = useState({});
+
+  const getUser = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `bearer ${localStorage.getItem("token")}`,
+      },
+    };
+    const response = await fetch(
+      "https://social-network-api.osc-fr1.scalingo.io/friend-net/user",
+      options
+    );
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser, console.log("user : ", user);
+  }, [user]);
+
   return (
     <div>
       <div className="menuWrapper">
         <Menu />
+      </div>
+      <button onClick={getUser}>User</button>
+      <div>
+        <p>{user.firstname}</p>
+        <p>{user.lastname}</p>
+        <p>{user.email}</p>
+        <p>{user.age}</p>
+        <p>{user.occupation}</p>
       </div>
       <div className="footerWrapper">
         <Footer />
